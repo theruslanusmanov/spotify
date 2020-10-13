536dd943103ce2f11fb159d429b1db804b81e2ec
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-10-13 16:32:14 +0500

    update(navbar): no-playing-bar

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -108,15 +108,6 @@ export const Navbar: React.FC = () => {
             </div>
             <span>Create Playlist</span>
           </li>
-          <li>
-            <div className="add">
-              <svg className="c0c4eafc5a90133e275bc12802a9c836-scss"
-                   shape-rendering="crispEdges" viewBox="0 0 36 36">
-                <path d="m28 20h-8v8h-4v-8h-8v-4h8v-8h4v8h8v4z"></path>
-              </svg>
-            </div>
-            <span>Liked Songs</span>
-          </li>
         </ul>
       </div>
     </div>
