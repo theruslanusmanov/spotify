c66388db5b72a4a97a4add9237f8e3fd07fad993
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-10-21 11:49:45 +0500

    Add authorization service

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.scss'
 import { Navbar } from './features/navbar/Navbar'
 import { MainView } from './features/main-view/MainView'
 import { TopBar } from './features/top-bar/TopBar'
 import { NowPlayingBar } from './features/now-playing-bar/NowPlayingBar'
+import AuthorizationService from './services/authorization-service'
 
 function App () {
+
+  useEffect(() => {
+    const authService = new AuthorizationService();
+    authService.authorize().then(r => console.log(r));
+  }, [])
+
   return (
     <div className="top-container">
       <TopBar></TopBar>
--- src/features/top-bar/TopBar.tsx
+++ src/features/top-bar/TopBar.tsx
@@ -4,6 +4,22 @@ import './TopBar.scss'
 export const TopBar: React.FC = () => {
   return (
     <div className="top-bar">
+      <button>
+        <svg role="img" focusable="false" height="24" width="24"
+             viewBox="0 0 24 24"
+             className="Svg-sc-1usfroi-0 fOBQdj _6fe5d5efc9b4a07d5c424071ac7cdacb-scss">
+          <polyline points="16 4 7 12 16 20" fill="none"
+                    stroke="#fff"></polyline>
+        </svg>
+      </button>
+      <button>
+        <svg role="img" focusable="false" height="24" width="24"
+             viewBox="0 0 24 24"
+             className="Svg-sc-1usfroi-0 fOBQdj _6fe5d5efc9b4a07d5c424071ac7cdacb-scss">
+          <polyline points="8 4 17 12 8 20" fill="none"
+                    stroke="#fff"></polyline>
+        </svg>
+      </button>
     </div>
   )
 }
