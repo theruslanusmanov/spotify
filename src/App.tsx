99b60c592e67ad6ea3b629b9a6b78744cf26d2dc
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-11-27 00:04:06 +0500

    Refactor store

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,25 @@
 import React, { useEffect } from 'react'
 import './App.scss'
-import { connect, ConnectedProps } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Navbar from './components/navbar/Navbar'
 import { MainView } from './components/main-view/MainView'
 import { TopBar } from './components/top-bar/TopBar'
 import NowPlayingBar from './components/now-playing-bar/NowPlayingBar'
 import { RootState } from './store/root.state'
-import { setToken } from './store/user/user.actions'
 import getTrackSelector from './store/spotify/spotify.selectors'
 import getAudioTrack from './store/player/player.selectors'
-import {spotifyActions} from './store/spotify'
+import { spotifyActions } from './store/spotify'
+import { userActions } from './store/user'
 
-const mapState = (state: RootState) => ({
-  token: state.user.token,
-  song: getTrackSelector(state),
-  isPlaying: state.player.isPlaying,
-  audioTrack: getAudioTrack(state),
-})
+function App () {
+  const dispatch = useDispatch()
 
-const mapDispatch = {
-  setToken: (token: string) => setToken(token),
-  playSong: (token: string) => spotifyActions.playSong(token),
-  fetchSongs: (token: string) => spotifyActions.fetchSongs(token),
-  loadPlaylists: (token: string) => spotifyActions.loadPlaylists(token),
-}
-
-const connector = connect(mapState, mapDispatch)
-
-type PropsFromRedux = ConnectedProps<typeof connector>
-
-type Props = PropsFromRedux
-
-function App (props: Props) {
-  const {
-    isPlaying,
-    audioTrack,
-    setToken,
-    playSong,
-    fetchSongs,
-    loadPlaylists
-  } = props
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const token = useSelector((state: RootState) => state.user.token)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const song = useSelector((state: RootState) => getTrackSelector(state))
+  const isPlaying = useSelector((state: RootState) => state.player.isPlaying)
+  const audioTrack = useSelector((state: RootState) => getAudioTrack(state))
 
   useEffect(() => {
     const hashParams: any = {}
@@ -53,10 +33,10 @@ function App (props: Props) {
     if (!hashParams.access_token) {
       window.location.href = 'https://accounts.spotify.com/authorize?client_id=230be2f46909426b8b80cac36446b52a&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback'
     } else {
-      setToken(hashParams.access_token)
-/*      playSong(hashParams.access_token)
-      fetchSongs(hashParams.access_token)*/
-      loadPlaylists(hashParams.access_token)
+      dispatch(userActions.setToken(hashParams.access_token))
+      /*      playSong(hashParams.access_token)
+            fetchSongs(hashParams.access_token)*/
+      dispatch(spotifyActions.loadPlaylists(hashParams.access_token))
     }
   }, [])
 
@@ -78,4 +58,4 @@ function App (props: Props) {
   )
 }
 
-export default connector(App)
+export default App
