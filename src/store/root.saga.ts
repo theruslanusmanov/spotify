9298470bbbf0833546813b33829be2e3f640e6e2
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-12-28 18:57:27 +0500

    Fix auth - get token

--- src/store/root.saga.ts
+++ src/store/root.saga.ts
@@ -1,10 +1,8 @@
 import {all, fork} from 'redux-saga/effects'
 import {spotifySagas} from './spotify/spotify.saga'
 import {playerSagas} from './player/player.saga'
-import {authorizationSagas} from "./authorization/authorization.saga";
 
 export const sagas = [
-  ...authorizationSagas,
   ...spotifySagas,
   ...playerSagas,
 ]
