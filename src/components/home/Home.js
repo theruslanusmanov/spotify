a3f908aa67be725d4ae8a995bc8714a702dd3398
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-10-01 20:36:52 +0500

    add(home): playlists template

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,5 +4,13 @@ import './Home.css';
 export const Home = () => (
   <div className="home">
     <h2>Good morning</h2>
+    <div className="grid-container">
+      <div className="playlist-card"></div>
+      <div className="playlist-card"></div>
+      <div className="playlist-card"></div>
+      <div className="playlist-card"></div>
+      <div className="playlist-card"></div>
+      <div className="playlist-card"></div>
+    </div>
   </div>
 )
