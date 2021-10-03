77d38645479a37db7a099cc68e7971d87becaa81
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-10-03 20:01:25 +0500

    add func to check public url

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css'
 import { Route } from "wouter";
 import { Shell } from './components/shell/Shell'
 import { Auth } from './components/auth/Auth'
+import { useEffect } from 'react'
 
 function App () {
+  useEffect(() => {
+    console.log(process.env.PUBLIC_URL)
+  })
+
   return (
     <div className="App">
       <Route path="/" component={Shell} />
