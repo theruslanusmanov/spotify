a54bd48ac43cd71ec32dc629fe8433f85da41675
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-10-04 12:24:32 +0500

    add(api): recommendations

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Home.css';
+import {PersonalizationApi} from '../../api/personalization.api';
 
-export const Home = () => (
-  <div className="home">
-    <h2>Good morning</h2>
-    <div className="grid-container">
-      <div className="playlist-card"></div>
-      <div className="playlist-card"></div>
-      <div className="playlist-card"></div>
-      <div className="playlist-card"></div>
-      <div className="playlist-card"></div>
-      <div className="playlist-card"></div>
-    </div>
-  </div>
-)
+export const Home = () => {
+  useEffect(() => {
+    PersonalizationApi.getShortcuts()
+      .then(response => console.log(response));
+  }, [])
+
+  return (
+      <div className="home">
+        <h2>Good morning</h2>
+        <div className="grid-container">
+          <div className="playlist-card"></div>
+          <div className="playlist-card"></div>
+          <div className="playlist-card"></div>
+          <div className="playlist-card"></div>
+          <div className="playlist-card"></div>
+          <div className="playlist-card"></div>
+        </div>
+      </div>
+  )
+}
