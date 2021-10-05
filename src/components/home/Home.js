aa4b1cdbe37973cdc502b24087706238fdc82d45
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-10-05 10:45:21 +0500

    update(home): load recommended albums

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,27 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './Home.css';
 import {PersonalizationApi} from '../../api/personalization.api';
 import {PlaylistCard} from '../playlist-card/PlaylistCard';
 
 export const Home = () => {
+  const [response, setResponse] = useState();
+
   useEffect(() => {
-    PersonalizationApi.getShortcuts()
-      .then(response => console.log(response));
-  }, [])
+    PersonalizationApi.getShortcuts().then(response => {
+      setResponse(response);
+    });
+  }, []);
 
   return (
       <div className="home">
         <h2>Good morning</h2>
         <div className="grid-container">
-          <PlaylistCard/>
+          {
+            response?.tracks?.map(function(data, i) {
+              return <PlaylistCard album={data.album} key={i}/>;
+            })
+          }
         </div>
       </div>
-  )
-}
+  );
+};
