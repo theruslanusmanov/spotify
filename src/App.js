b2ea9defac3489b4a130fd9d826495cde2bc0e30
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-10-14 22:41:55 +0500

    add: search component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import {Route} from 'react-router-dom';
 import {Shell} from './components/shell/Shell';
 import {Auth} from './components/auth/Auth';
+import {Search} from "./components/search/Search";
 
 function App() {
   return (
       <div className="App">
+        <Route path="/search" component={Search}/>
         <Route path="/auth" component={Auth}/>
         <Route path="/" component={Shell} exact />
       </div>
---
+++ src/components/search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Search.css';
+
+export const Search = () => {
+
+  return (
+      <div className="search">
+        <h1>Search works!</h1>
+      </div>
+  );
+};
