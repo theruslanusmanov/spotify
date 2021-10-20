92c36b6b7fda4e453063d5f309c82ac07a02e6a4
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-10-20 21:03:17 +0500

    add(search): genre cards grid

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -5,7 +5,21 @@ export const Search = () => {
 
   return (
       <div className="search">
-        <h1>Search works!</h1>
+        <h1>All genres</h1>
+        <div className="grid-container">
+          <div className="genre-card">
+            <h3>Rock</h3>
+          </div>
+          <div className="genre-card">
+            <h3>Rock</h3>
+          </div>
+          <div className="genre-card">
+            <h3>Rock</h3>
+          </div>
+          <div className="genre-card">
+            <h3>Rock</h3>
+          </div>
+        </div>
       </div>
   );
 };
