d836be76b53bfb8bd5d5f74e1ccd6cca37210597
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-10-21 21:51:58 +0500

    add lib button in sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,6 +3,7 @@ import './Sidebar.css'
 import { ReactComponent as SpotifyLogo } from './spotify-logo.svg'
 import { ReactComponent as Home } from './home.svg'
 import { ReactComponent as Search } from './search.svg'
+import { ReactComponent as Library } from './library.svg'
 
 export const Sidebar = () => (
   <div className="sidebar">
@@ -15,6 +16,9 @@ export const Sidebar = () => (
       </li>
       <li>
         <a href="/search"><Search/><span>Search</span></a>
+      </li>
+      <li>
+        <a href="/collection"><Library/><span>Your Library</span></a>
       </li>
     </ul>
   </div>
