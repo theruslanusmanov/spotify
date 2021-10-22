22441fade041862146a7ebb7c1268ca1ffe7400e
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-10-22 16:31:35 +0500

    add library page

---
+++ src/components/library/Library.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Library.css';
+
+export const Library = () => {
+
+  return (
+      <div className="search">
+        <h1>Library</h1>
+        <div className="grid-container">
+          <div className="genre-card">
+            <h3>Rock</h3>
+          </div>
+          <div className="genre-card">
+            <h3>Rock</h3>
+          </div>
+          <div className="genre-card">
+            <h3>Rock</h3>
+          </div>
+          <div className="genre-card">
+            <h3>Rock</h3>
+          </div>
+        </div>
+      </div>
+  );
+};
--- src/components/shell/Shell.js
+++ src/components/shell/Shell.js
@@ -1,42 +1,44 @@
-import React, {useEffect, useState} from 'react';
-import {Route} from 'react-router-dom';
-import './Shell.css';
-import {Sidebar} from '../sidebar/Sidebar';
-import {Home} from '../home/Home';
-import {TopBar} from '../top-bar/TopBar';
-import {AuthorizationService} from '../../services/authorization.service';
-import {Search} from '../search/Search';
+import React, { useEffect, useState } from 'react'
+import { Route } from 'react-router-dom'
+import './Shell.css'
+import { Sidebar } from '../sidebar/Sidebar'
+import { Home } from '../home/Home'
+import { TopBar } from '../top-bar/TopBar'
+import { AuthorizationService } from '../../services/authorization.service'
+import { Search } from '../search/Search'
+import { Library } from '../library/Library'
 
 export const Shell = () => {
-  const [token, setToken] = useState();
+  const [token, setToken] = useState()
 
   useEffect(() => {
-    const savedToken = localStorage.getItem('access_token');
+    const savedToken = localStorage.getItem('access_token')
 
-    console.log(savedToken);
+    console.log(savedToken)
     if (!savedToken && savedToken == null) {
-      AuthorizationService.authorize();
+      AuthorizationService.authorize()
     }
 
-    setToken(savedToken);
-  }, []);
+    setToken(savedToken)
+  }, [])
 
   return (
-      <div className="shell">
-        {
-          token
-              ? (
-                  <>
-                    <Sidebar/>
-                    <TopBar/>
-                    <div className="container">
-                      <Route path="/search" component={Search}/>
-                      <Route exact path="/" component={Home}/>
-                    </div>
-                  </>
-              )
-              : null
-        }
-      </div>
-  );
-};
+    <div className="shell">
+      {
+        token
+          ? (
+            <>
+              <Sidebar/>
+              <TopBar/>
+              <div className="container">
+                <Route path="/collection" component={Library}/>
+                <Route path="/search" component={Search}/>
+                <Route exact path="/" component={Home}/>
+              </div>
+            </>
+          )
+          : null
+      }
+    </div>
+  )
+}
