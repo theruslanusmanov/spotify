cea8174741b5715427db886c76f6a095ea21ac07
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-10-25 20:19:24 +0500

    add(library): playlist card

--- src/components/library/Library.js
+++ src/components/library/Library.js
@@ -7,17 +7,10 @@ export const Library = () => {
       <div className="search">
         <h1>Library</h1>
         <div className="grid-container">
-          <div className="genre-card">
+          <div className="playlist-card">
+            <div className="playlist-card__cover"></div>
             <h3>Rock</h3>
-          </div>
-          <div className="genre-card">
-            <h3>Rock</h3>
-          </div>
-          <div className="genre-card">
-            <h3>Rock</h3>
-          </div>
-          <div className="genre-card">
-            <h3>Rock</h3>
+            <h4>Rock legends & epic songs spanning decades, that continue to inspire generations.</h4>
           </div>
         </div>
       </div>
