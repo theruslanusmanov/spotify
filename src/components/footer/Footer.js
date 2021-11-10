4f7a457337bc56862bf07b857a9374db4ff1d24e
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-11-10 06:00:48 +0500

    update(footer): add player controls

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Footer.css'
+import { ReactComponent as Previous } from './previous.svg'
+import { ReactComponent as Play } from './play.svg'
+import { ReactComponent as Next } from './next.svg'
 
 export const Footer = () => {
 
@@ -9,7 +12,17 @@ export const Footer = () => {
           <div className="player-info__cover"></div>
           <div className="player-info__name">Song Name</div>
         </div>
-        <div className="player-controls"></div>
+        <div className="player-controls">
+          <div className="previous-button">
+            <Previous/>
+          </div>
+          <div className="play-button">
+            <Play/>
+          </div>
+          <div className="next-button">
+            <Next/>
+          </div>
+        </div>
         <div className="player-extra-controls"></div>
       </footer>
   );
