61b9d8e342aa3f9a205884b7738bb3467c0d71b7
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-11-11 06:56:37 +0500

    update sidebar links to navlink

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,25 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './Sidebar.css'
 import { ReactComponent as SpotifyLogo } from './spotify-logo.svg'
 import { ReactComponent as Home } from './home.svg'
 import { ReactComponent as Search } from './search.svg'
 import { ReactComponent as Library } from './library.svg'
 
-export const Sidebar = () => (
-  <div className="sidebar">
-    <div className="logo">
-      <SpotifyLogo/>
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="logo">
+        <SpotifyLogo/>
+      </div>
+      <ul>
+        <li>
+          <NavLink to="/" exact activeClassName="active"><Home/><span>Home</span></NavLink>
+        </li>
+        <li>
+          <NavLink to="/search" activeClassName="active"><Search/><span>Search</span></NavLink>
+        </li>
+        <li>
+          <NavLink to="/collection" activeClassName="active"><Library/><span>Your Library</span></NavLink>
+        </li>
+      </ul>
     </div>
-    <ul>
-      <li>
-        <a href="/" className="active"><Home/><span>Home</span></a>
-      </li>
-      <li>
-        <a href="/search"><Search/><span>Search</span></a>
-      </li>
-      <li>
-        <a href="/collection"><Library/><span>Your Library</span></a>
-      </li>
-    </ul>
-  </div>
-)
+  )
+}
