65a743321d28d695825b47f4c7f46973f10c8a88
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-11-13 09:05:26 +0500

    cypress: add Home test

---
+++ cypress/integration/home/home.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Home', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('displays two todo items by default', () => {
+  })
+})
