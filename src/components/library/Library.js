091c8146d6330ac8561fa9bf68b6a2815ead1a6b
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-11-26 22:29:58 +0500

    remove console.log

--- src/components/library/Library.js
+++ src/components/library/Library.js
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from 'react'
-import './Library.css';
+import './Library.css'
 import { PlaylistsApi } from '../../api/playlists.api'
 
 export const Library = () => {
-  const [playlists, setPlaylists] = useState([]);
+  const [playlists, setPlaylists] = useState([])
 
   useEffect(() => {
     PlaylistsApi.getCurrentUserPlaylist(10, 0).then(response => {
-      console.log(response);
-      setPlaylists(response.items);
-    });
+      setPlaylists(response.items)
+    })
   }, [])
 
   return (
-      <div className="search">
-        <h1>Library</h1>
-        <div className="grid-container">
-          <div className="playlist-card">
-            <div className="playlist-card__cover"></div>
-            <h3>Rock</h3>
-            <h4>Rock legends & epic songs spanning decades, that continue to inspire generations.</h4>
-          </div>
-          {
-            playlists.map((playlist, key) => {
-              return <div className="playlist-card" key={key}>
-                <div className="playlist-card__cover"></div>
-                <h3>{playlist.name}</h3>
-                <h4>{playlist.description}</h4>
-              </div>
-            })
-          }
+    <div className="search">
+      <h1>Library</h1>
+      <div className="grid-container">
+        <div className="playlist-card">
+          <div className="playlist-card__cover"></div>
+          <h3>Rock</h3>
+          <h4>Rock legends & epic songs spanning decades, that continue to
+            inspire generations.</h4>
         </div>
+        {
+          playlists.map((playlist, key) => {
+            return <div className="playlist-card" key={key}>
+              <div className="playlist-card__cover"></div>
+              <h3>{playlist.name}</h3>
+              <h4>{playlist.description}</h4>
+            </div>
+          })
+        }
       </div>
-  );
-};
+    </div>
+  )
+}
