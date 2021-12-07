f48b5c1759c0ac0c4c7c197e2c3cb1e76e0e8c8a
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-12-07 23:23:50 +0500

    add hash router

--- src/services/authorization.service.js
+++ src/services/authorization.service.js
@@ -2,7 +2,7 @@ const isProd = process.env.REACT_APP_ENV === 'prod'
 const CLIENT_ID = '6e313238e1804ca3891543ae4f2b0ee3'
 const REDIRECT_URI = isProd
   ? 'https://theruslanusmanov.github.io/spotify/auth'
-  : 'http://localhost:3000/auth'
+  : 'http://localhost:3000/#/auth'
 
 // Change the scopes to what you need
 // https://developer.spotify.com/documentation/general/guides/scopes/
